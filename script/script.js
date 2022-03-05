982b4c85aeb0aeb96a6f56e7bb6b6c57736877e2
Shanty <codeacademy22214@gmail.com>
2022-03-05 23:39:52 +0100

    v1.0

---
+++ script/script.js
@@ -0,0 +1,17 @@
+
+let menu = document.querySelector('#menu-bar');
+let navbar = document.querySelector('.navbar')
+
+
+window.onscroll = () => {
+    searchBtn.classList.remove('fa-times');
+    searchBar.classList.remove('active');
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
+
+menu.addEventListener('click', () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+    
+});
