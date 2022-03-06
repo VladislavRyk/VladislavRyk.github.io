3ecbc1416167f86724fccccdeaf96eadc5e4f52b
Shanty <codeacademy22214@gmail.com>
2022-03-06 21:25:11 +0100

    fix header & home section

--- script/script.js
+++ script/script.js
@@ -9,9 +9,3 @@ window.onscroll = () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 }
-
-menu.addEventListener('click', () =>{
-    menu.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-    
-});
